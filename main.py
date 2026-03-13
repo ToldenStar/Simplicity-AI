48582515d105dc0ce96f2d1bbae74af79fead0e1
ToldenStar <nathenskaggs@proton.me>
2026-03-13 17:59:03 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a simple AI it may improved in the future.
+
+
+# Respond function
+def Respond():
+    print("Test")
+
+    response = input("Say Hi or Bye: ")
+
+    if response == "Hi":
+        print("Hi")
+
+    if response == "Bye":
+        print("Bye") 
+
+Respond()
